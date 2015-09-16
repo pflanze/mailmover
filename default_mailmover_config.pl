89c7899348a118bd08cee24ca4aa6440c119ff8b
Christian Jaeger <ch@christianjaeger.ch>
2015-09-16 08:31:54 +0100

    add own_emailaddress as configuration for from address for replies

--- default_mailmover_config.pl
+++ default_mailmover_config.pl
@@ -1,6 +1,13 @@
+# detect all possible addresses that are your own!
 sub is_own_emailaddress {
     my ($from)=@_;
     0
+}
+
+# one email address that is to be used for sending autoreply (and
+# perhaps other) mails
+sub own_emailaddress {
+    'XXX@YYY.org'
 }
 
 1
