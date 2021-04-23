988d8dd68e592b9e16e1efd28d548e60b32f2dd2
Christian Jaeger <ch@christianjaeger.ch>
2021-04-23 13:03:33 +0100

    Lib: move pre-condition for formula validity out of formula
    Now it fits into 1 line, too.

--- lib/Mailmover/Lib.pm
+++ lib/Mailmover/Lib.pm
@@ -139,12 +139,8 @@ sub classify {
 	      # bad things in lists per se)
               my $high_spamscore = '$spamscore > (1 + $specific_list_allowance)';
               my $mix_with_old =
-                  'defined $maybe_spamscore_old and
-		   $spamscore <= 1 and
-		   (2*($spamscore-1) + $maybe_spamscore_old)
-		   > $specific_list_allowance*2
-		   ';
-              $mix_with_old=~ s/\n\s+/\n/sg;
+                  '$spamscore <= 1 and (2*($spamscore-1) + $maybe_spamscore_old) > $specific_list_allowance*2';
+              # $mix_with_old=~ s/\n\s+/\n/sg;
 
               my $show_with_interpol= sub {
                   my ($which, $formula)= @_;
@@ -154,7 +150,7 @@ sub classify {
               
               if (eval($high_spamscore) // die $@) {
                   $show_with_interpol->("high_spamscore", $high_spamscore)
-	      } elsif (eval($mix_with_old) // die $@) {
+	      } elsif (defined $maybe_spamscore_old and eval($mix_with_old) // die $@) {
                   $show_with_interpol->("mix_with_old", $mix_with_old)
 	      } else {
 		  ''
